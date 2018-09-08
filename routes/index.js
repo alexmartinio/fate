180f9bf33308e2cc4c6c2dae51af5cdbe81f71d1
ANT290 <Ant290@live.co.uk>
2018-09-08 23:44:47 +0100

    prittier reccomendations

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fate lies' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Fate times" });
 });
 
 module.exports = router;
