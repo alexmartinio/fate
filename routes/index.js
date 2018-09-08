cd5d2894871d288d1fe6598d624e93601d8c151b
ANT290 <Ant290@live.co.uk>
2018-09-08 23:38:55 +0100

    Revert "updated the index title"
    This reverts commit 1ea2b809f57e6c48bdc64d3aea9e391028f95be7.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Fate lies' });
+  res.render('index', { title: 'Fate times' });
 });
 
 module.exports = router;
